15e84fb3cb7c4113e829f1f15d51f2e804a95d2f
TheDxns <denis.lukasczyk@gmail.com>
2021-08-07 22:43:57 +0200

    created user entity

---
+++ src/main/java/io/github/thedxns/blog/model/User.java
@@ -0,0 +1,98 @@
+package io.github.thedxns.blog.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotBlank(message = "The username must not be empty")
+    @Column(name = "username")
+    private String username;
+
+    @JsonIgnore
+    @NotBlank(message = "The password must not be empty")
+    @Column(name = "password")
+    private String password;
+
+    @NotBlank(message = "The name of the user must not be empty")
+    @Column(name = "name")
+    private String name;
+
+    @NotBlank(message = "The surname of the user must not be empty")
+    @Column(name = "surname")
+    private String surname;
+
+    @NotBlank(message = "The e-mail address of the user must not be empty")
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "subscriber_status")
+    private boolean subscriberStatus;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isSubscriberStatus() {
+        return subscriberStatus;
+    }
+
+    public void setSubscriberStatus(boolean subscriberStatus) {
+        this.subscriberStatus = subscriberStatus;
+    }
+}
