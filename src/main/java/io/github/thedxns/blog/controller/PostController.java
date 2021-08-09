8932112b9f1f4d748ebbcb056e6331de8437e086
TheDxns <denis.lukasczyk@gmail.com>
2021-08-09 21:43:53 +0200

    created basic create and read methods for post controller

--- src/main/java/io/github/thedxns/blog/controller/PostController.java
+++ src/main/java/io/github/thedxns/blog/controller/PostController.java
@@ -1,11 +1,44 @@
 package io.github.thedxns.blog.controller;
 
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.thedxns.blog.logic.PostService;
+import io.github.thedxns.blog.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getPosts() {
+        return ResponseEntity.ok(postService.getAllPosts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getPostById(@PathVariable int id) {
+        return ResponseEntity.ok(postService.getPost(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> savePost(@Valid @RequestBody Post post) {
+        if (postService.savePost(post)) {
+            return new ResponseEntity<String>("The post has been published.", HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<String>("The post has not been published. Try again.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 
 }
