aaa86ff052dbdfbdea032a72542ab0a3c5b68cff
TheDxns <denis.lukasczyk@gmail.com>
2021-08-13 12:03:28 +0200

    created comment entity

--- src/main/java/io/github/thedxns/blog/model/Comment.java
+++ src/main/java/io/github/thedxns/blog/model/Comment.java
@@ -1,6 +1,75 @@
 package io.github.thedxns.blog.model;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
 @Entity
 public class Comment {
-    
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotBlank(message = "The content of the comment must not be empty")
+    @Column(length = 100000)
+    private String content;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Post post;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User creator;
+
+    private LocalDateTime createdOn;
+
+    private LocalDateTime updatedOn;
+
+    public Comment() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setText(String content) {
+        this.content = content;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    @PrePersist
+    void prePersist() {
+        createdOn = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    void preUpdate() {
+        updatedOn = LocalDateTime.now();
+    }
 }
