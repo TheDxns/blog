f85965f094f277e7debb0cfaa34a5f13729afa7d
TheDxns <denis.lukasczyk@gmail.com>
2021-08-13 09:16:15 +0200

    moved repo to another package

--- src/main/java/io/github/thedxns/blog/model/repositories/PostRepository.java
+++ src/main/java/io/github/thedxns/blog/model/repositories/PostRepository.java
@@ -1,7 +1,9 @@
-package io.github.thedxns.blog.model;
+package io.github.thedxns.blog.model.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import io.github.thedxns.blog.model.Post;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
