3d9aa34562504e0637713f32f8174dc2538ca8f2
TheDxns <denis.lukasczyk@gmail.com>
2021-08-19 23:06:33 +0200

    added home view

--- src/main/java/io/github/thedxns/blog/controller/HomeController.java
+++ src/main/java/io/github/thedxns/blog/controller/HomeController.java
@@ -1,8 +1,13 @@
 package io.github.thedxns.blog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    
+
+    @GetMapping("/")
+    public String showHomePage() {
+        return "index";
+    }
 }
