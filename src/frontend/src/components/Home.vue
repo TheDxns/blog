728d1b1306a7994da7184a5fd538908c191b28e3
TheDxns <denis.lukasczyk@gmail.com>
2021-08-22 21:42:53 +0200

    switched from Vue 3 to Vue 2 because of Vuetify support

--- src/frontend/src/components/Home.vue
+++ src/frontend/src/components/Home.vue
@@ -1,48 +1,32 @@
 <template>
-  <div class="home">
-    <h1>Blog</h1>
-    <h3>Post list:</h3>
-    <ul>
-      <template v-for="post in posts" :key="post.content">
-        <li>{{post.id}}, {{post.title}}, {{post.content}}</li>
-        <br />
-      </template>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <h1>Blog</h1>
+        <h3>Post list:</h3>
+        <ul>
+            <li v-for="post in posts" :key="post.content">{{post.id}}, {{post.title}}, {{post.content}}</li>
+            <br />
+        </ul>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'Home',
-  data() {
-    return {
-      posts: []
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        posts: []
+      }
+    },
+    mounted() {
+      fetch("/api/posts")
+          .then((response) => response.json())
+          .then((data) => {
+            this.posts = data;
+          })
     }
-  },
-  mounted() {
-    fetch("/api/posts")
-      .then((response) => response.json())
-        .then((data) => {
-          this.posts = data;
-        })
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
