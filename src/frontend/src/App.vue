8cdc4812ce9dd2f7aa3b057aa1259c74d774b0be
TheDxns <denis.lukasczyk@gmail.com>
2021-08-23 22:34:26 +0200

    updated main App to use the added footer and navbar

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,43 +1,22 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <h1>Blog by TheDxns</h1>
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="#"
-        text
-      >
-        <span class="mr-2">Login</span>
-        <v-icon>mdi-account</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <Home/>
+      <Navbar/>
+      <router-view/>
+      <Footer/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Home from './components/Home.vue'
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name: 'App',
-
   components: {
-    Home
-  },
-
-  data: () => ({
-    //
-  }),
-};
+    Navbar,
+    Footer
+  }
+}
 </script>
