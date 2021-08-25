b336576d76d2cd8643a0bb4e6f8e110585acd9af
TheDxns <denis.lukasczyk@gmail.com>
2021-08-25 21:28:55 +0200

    updated about text

--- src/frontend/src/views/About.vue
+++ src/frontend/src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container class="mx-16">
+    <v-row class="mx-16">
+      <v-col cols="12" class="mx-16">
+        <h1 class="mt-6 ml-16">About:</h1>
+          <h2 class="mt-6 ml-16">Personal blog created in Spring Boot, Vue.js and Vuetify.</h2>
+          <h3 class="mt-6 ml-16 red--text">Work in progress...</h3>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
