fee7a5966df1cdb231e09ed91cb138c2e63f7097
TheDxns <denis.lukasczyk@gmail.com>
2021-08-25 21:29:21 +0200

    updated contact page description

--- src/frontend/src/views/Contact.vue
+++ src/frontend/src/views/Contact.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="contact">
-    <h1>This is a contact page</h1>
-  </div>
+  <v-container class="mx-16">
+    <v-row class="mx-16">
+      <v-col cols="12" class="mx-16">
+        <h1 class="mt-6 ml-16">Contact:</h1>
+        <h2 class="mt-6 ml-16">Here you will find a contact form and an option to subscribe to the blog.</h2>
+        <h3 class="mt-6 ml-16 red--text">Work in progress...</h3>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
