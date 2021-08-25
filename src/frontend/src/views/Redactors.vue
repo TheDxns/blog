e4274046782aae1c6558cfef7a7f4663fe4173a0
TheDxns <denis.lukasczyk@gmail.com>
2021-08-25 21:29:45 +0200

    updated redactor page description

--- src/frontend/src/views/Redactors.vue
+++ src/frontend/src/views/Redactors.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="redactors">
-    <h1>This is a redactors page</h1>
-  </div>
+  <v-container class="mx-16">
+    <v-row class="mx-16">
+      <v-col cols="12" class="mx-16">
+        <h1 class="mt-6 ml-16">Contact:</h1>
+        <h2 class="mt-6 ml-16">Here you will a list of all blog redactors.</h2>
+        <h3 class="mt-6 ml-16 red--text">Work in progress...</h3>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
