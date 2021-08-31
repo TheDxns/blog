97e1c4df4add10fdf17ebf15f97db12f7cb78d0a
TheDxns <denis.lukasczyk@gmail.com>
2021-08-31 15:25:27 +0200

    recreated post design

--- src/frontend/src/components/Post.vue
+++ src/frontend/src/components/Post.vue
@@ -1,44 +1,53 @@
 <template>
       <v-card
-          class="mx-auto my-10 blue-grey darken-2"
-          dark
-          width="1000"
-      >
-        <v-card-title class="ml-5">
-          <span class="headline font-weight-bold">{{ this.post.title }}</span>
-        </v-card-title>
-
-        <v-card-text class="font-weight-light ml-5">
-          {{ this.post.sneakPeak }}
-        </v-card-text>
-
-        <v-card-actions>
-          <v-list-item class="grow ml-5">
-            <v-list-item-avatar color="grey darken-3">
-              <v-img
-                  class="elevation-6"
-                  src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortWaved&accessoriesType=Prescription02&hairColor=BrownDark&facialHairType=Blank&clotheType=Hoodie&clotheColor=Blue03&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Twinkle&skinColor=Light"
-              ></v-img>
-            </v-list-item-avatar>
-
-            <v-list-item-content>
-              <v-list-item-title>{{this.name}}</v-list-item-title>
-            </v-list-item-content>
-
-            <v-row
-                align="center"
-                justify="end"
-                class="mr-10"
+      class="mx-auto mt-10"
+      max-width="344"
+      outlined
+      tile
+    >
+      <v-card-text>
+        <div>{{this.post.category}}</div>
+        <p class="text-h4 text--primary">
+          {{ this.post.title }}
+        </p>
+        <p>{{ this.post.creator }}</p>
+        <div class="text--primary">
+          {{this.post.sneakPeak}}
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          text
+          color="teal accent-4"
+        >
+          Read post
+        </v-btn>
+      </v-card-actions>
+  
+      <v-expand-transition>
+        <v-card
+          v-if="reveal"
+          class="transition-fast-in-fast-out v-card--reveal"
+          style="height: 100%;"
+        >
+          <v-card-text class="pb-0">
+            <p class="text-h4 text--primary">
+              Origin
+            </p>
+            <p>late 16th century (as a noun denoting a place where alms were distributed): from medieval Latin eleemosynarius, from late Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’ </p>
+          </v-card-text>
+          <v-card-actions class="pt-0">
+            <v-btn
+              text
+              color="teal accent-4"
+              @click="reveal = false"
             >
-              <v-icon class="mr-1">mdi-heart</v-icon>
-              <span class="subheading mr-2">256</span>
-              <span class="mr-1">·</span>
-              <v-icon class="mr-1">mdi-comment</v-icon>
-              <span class="subheading">45</span>
-            </v-row>
-          </v-list-item>
-        </v-card-actions>
-      </v-card>
+              Close
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-expand-transition>
+    </v-card>
 </template>
 
 <script>
