9fb900e960dd2fd57f7dbf8fcaf58e77d422d62e
TheDxns <denis.lukasczyk@gmail.com>
2021-08-31 15:24:23 +0200

    added links to footer icons

--- src/frontend/src/components/Footer.vue
+++ src/frontend/src/components/Footer.vue
@@ -13,18 +13,28 @@
       <v-card-title class="font-weight-light">
         <strong class="subheading"></strong>
         <v-spacer></v-spacer>
-
-        <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            class="mx-4"
-            dark
-            icon
-        >
-          <v-icon size="24px">
-            {{ icon }}
-          </v-icon>
-        </v-btn>
+          <v-btn
+              class="mx-4"
+              dark
+              icon
+              href="https://github.com/TheDxns" 
+              target="_blank"
+          >
+            <v-icon size="24px">
+              mdi-github
+            </v-icon>
+          </v-btn>
+          <v-btn 
+              class="mx-4"
+              dark
+              icon
+              href="https://www.linkedin.com/in/denislukasczyk" 
+              target="_blank"
+          >
+            <v-icon size="24px">
+              mdi-linkedin
+            </v-icon>
+          </v-btn>
         <v-card-text class="py-0  white--text text-center">
         © {{ new Date().getFullYear() }} — <strong>TheDxns</strong>
         </v-card-text>
@@ -38,11 +48,5 @@
 <script>
 export default {
   name: 'Navbar',
-  data: () => ({
-    icons: [
-      'mdi-github',
-      'mdi-linkedin',
-    ],
-  }),
 }
 </script>
