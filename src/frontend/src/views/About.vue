519ebeaa95db447fcfa9d71f205a13b09f6f613a
TheDxns <denis.lukasczyk@gmail.com>
2021-09-09 12:58:27 +0200

    updated the information displayed on page

--- src/frontend/src/views/About.vue
+++ src/frontend/src/views/About.vue
@@ -2,9 +2,10 @@
   <v-container class="mx-16">
     <v-row class="mx-16">
       <v-col cols="12" class="mx-16">
-        <h1 class="mt-6 ml-16">About:</h1>
-          <h2 class="mt-6 ml-16">Personal blog created in Spring Boot, Vue.js and Vuetify.</h2>
-          <h3 class="mt-6 ml-16 red--text">Work in progress...</h3>
+        <h1 class="mt-6 ml-16">About the blog:</h1>
+          <h2 class="mt-6 ml-16">A personal blog created with use of Vue.js and Vuetify for the frontend and Spring Boot on the server side.</h2>
+          <h2 class="mt-6 ml-16">Keycloak is used as an authorization server with the OpenID Connect protocol.</h2>
+          <h3 class="mt-16 ml-16 red--text">The blog development is still in progress...</h3>
       </v-col>
     </v-row>
   </v-container>
