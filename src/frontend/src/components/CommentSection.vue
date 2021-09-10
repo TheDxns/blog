50d5f065a6660bf9dfc578aee146dd04b626ca75
TheDxns <denis.lukasczyk@gmail.com>
2021-09-10 15:47:12 +0200

    created comment section component

---
+++ src/frontend/src/components/CommentSection.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-row>
+    <v-col
+      cols="12"
+      sm="6"
+      offset-sm="3"
+    >
+        <Comment v-for="comment in comments" :key="comment.content" v-bind:comment="comment"/>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import Comment from '@/components/Comment.vue'
+
+  export default {
+    name: 'CommentSection',
+    data() {
+      return {
+        comments: []
+      }
+    },
+    components: {
+      Comment
+    },
+    mounted() {
+      fetch("/api/comments?sort=id,desc")
+          .then((response) => response.json())
+          .then((data) => {
+            this.comments = data;
+          })
+    }
+  }
+</script>
