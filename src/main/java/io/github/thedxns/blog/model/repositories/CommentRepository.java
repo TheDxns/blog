3cf6b4cb0cf3bfc8dfe967e04eb51fa76ebab4b4
TheDxns <denis.lukasczyk@gmail.com>
2021-09-10 15:50:37 +0200

    created comment repository

--- src/main/java/io/github/thedxns/blog/model/repositories/CommentRepository.java
+++ src/main/java/io/github/thedxns/blog/model/repositories/CommentRepository.java
@@ -1,12 +1,14 @@
 package io.github.thedxns.blog.model.repositories;
 
 import org.springframework.stereotype.Repository;
-
 import io.github.thedxns.blog.model.Comment;
-
+import org.springframework.data.domain.Pageable;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
-    
+
+    List<Comment> findByPostId(Integer postId);
+    List<Comment> findAllByPostId(Integer postId, Pageable pageable);
 }
