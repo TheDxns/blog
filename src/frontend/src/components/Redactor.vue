30fa4b16e130f6b39c62cfdcb38859f2d96bb95d
TheDxns <denis.lukasczyk@gmail.com>
2021-09-22 15:48:13 +0200

    minor font correction

---
+++ src/frontend/src/components/Redactor.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-card class="px-5 py-3">
+        <v-avatar>
+            <img
+                :src=redactor.avatarUrl
+                alt=""
+            >
+        </v-avatar>
+      <h3>{{redactor.firstname}} {{redactor.surname}}</h3>
+    </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Redactor',
+  props: ['redactor'],
+  data() {
+      return {
+        
+      }
+    },
+}
+</script>
--- src/frontend/src/views/About.vue
+++ src/frontend/src/views/About.vue
@@ -2,7 +2,7 @@
   <v-container class="mx-16">
     <v-row class="mx-16">
       <v-col cols="12" class="mx-16">
-        <h1 class="mt-6 ml-16">About the blog:</h1>
+        <h1 class="mt-6 ml-16 font-weight-light">About the blog:</h1>
           <h2 class="mt-6 ml-16">A personal blog created with use of Vue.js and Vuetify for the frontend and Spring Boot on the server side.</h2>
           <h2 class="mt-6 ml-16">Keycloak is used as an authorization server with the OpenID Connect protocol.</h2>
           <h3 class="mt-16 ml-16 red--text">The blog development is still in progress...</h3>
