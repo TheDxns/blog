e5fc8e4908fcf85820bae45ba438f89d54df206c
TheDxns <denis.lukasczyk@gmail.com>
2021-09-22 15:46:45 +0200

    improved design of the comments

--- src/frontend/src/components/Comment.vue
+++ src/frontend/src/components/Comment.vue
@@ -1,7 +1,25 @@
 <template>
-    <v-card class="px-5 py-3">
-      <h3>{{author}} - {{dateCreated}}</h3>
-      {{comment.content}} (ID: {{comment.id}})
+    <v-card flat class="px-5 py-3">
+       <v-chip
+       link
+        color="white"
+        >
+            <v-avatar left>
+              <img
+                  :src=avatarUrl
+                  alt=""
+              >
+            </v-avatar>
+            {{author}}
+          </v-chip>
+          <v-chip
+          label
+          class=""
+          color="white"
+          >
+                {{dateCreated}} 10:31
+          </v-chip>
+      <h6 class="font-weight-light ml-2">{{comment.content}} (ID: {{comment.id}})</h6>
       </v-card>
 </template>
 
@@ -13,7 +31,8 @@ export default {
       return {
         name: "",
         author: "Denis Lukasczyk",
-        dateCreated: "20.09.2021"
+        dateCreated: "20.09.2021",
+        avatarUrl: "https://cdn.vuetifyjs.com/images/john.jpg"
       }
     },
 }
