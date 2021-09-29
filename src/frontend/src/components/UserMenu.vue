6124d4437a24b6c68c1b27f90158e478d1ea0f6a
TheDxns <denis.lukasczyk@gmail.com>
2021-09-29 15:49:28 +0200

    added a user menu on the nav bar

---
+++ src/frontend/src/components/UserMenu.vue
@@ -0,0 +1,83 @@
+<template>
+      <v-menu
+        bottom
+        min-width="200px"
+        rounded
+        offset-y
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn
+            icon
+            x-large
+            v-on="on"
+          >
+            <v-avatar
+              color="grey"
+              size="48"
+            >
+              <span class="white--text text-h5">{{ user.initials }}</span>
+            </v-avatar>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-list-item-content class="justify-center">
+            <div class="mx-auto text-center">
+              <v-avatar
+                color="grey"
+              >
+                <span class="white--text text-h5">{{ user.initials }}</span>
+              </v-avatar>
+              <h3>{{ user.username }}</h3>
+              <p class="text-caption mt-1">
+                {{ user.email }}
+              </p>
+              <v-divider class="my-3"></v-divider>
+              <v-btn
+                href="/new-post"
+                depressed
+                rounded
+                text
+              >
+                New post
+              </v-btn>
+              <v-divider class="my-3"></v-divider>
+              <v-btn
+                depressed
+                rounded
+                text
+              >
+                Edit Account
+              </v-btn>
+              <v-divider class="my-3"></v-divider>
+              <v-btn
+                @click="logOut()"
+                depressed
+                rounded
+                text
+              >
+                Log out
+              </v-btn>
+            </div>
+          </v-list-item-content>
+        </v-card>
+      </v-menu>
+</template>
+
+<script>
+  export default {
+    name: "UserMenu",
+     data() {
+      return {
+      user: {
+        initials: 'JD',
+        email: 'john.doe@doe.com',
+        username: 'John Doe'
+      },
+    }
+    },
+    methods: {
+        logOut() {
+        }
+    }
+  }
+</script>
