24237af7be50bfefad829d77876e6036bc419913
TheDxns <denis.lukasczyk@gmail.com>
2021-10-01 15:45:28 +0200

    Improved page design by adding icons changing font format

--- src/frontend/src/views/About.vue
+++ src/frontend/src/views/About.vue
@@ -3,10 +3,25 @@
     <v-row class="mx-16">
       <v-col cols="10" class="mx-16">
         <h1 class="mt-6 ml-16 font-weight-light">About the blog:</h1>
-          <h2 class="mt-6 ml-16">A personal blog created with use of Vue.js and Vuetify for the frontend and Spring Boot on the server side.</h2>
-          <h2 class="mt-6 ml-16">Keycloak is used as an authorization server with the OpenID Connect protocol.</h2>
-          <h3 class="mt-16 ml-16 red--text">The blog development is still in progress...</h3>
+          <h2 class="mt-6 ml-16 font-weight-light">A personal blog created with use of <span class="green--text">Vue.js</span><img src="https://img.icons8.com/color/30/000000/vue-js.png"/> and 
+          <span class="blue--text">Vuetify</span><img src="https://img.icons8.com/windows/30/000000/vuetify.png"/> for the frontend and 
+          <span class="light-green--text"> Spring Boot</span> <img src="https://img.icons8.com/color/30/000000/spring-logo.png"/> on the server side.</h2>
+          <h2 class="mt-6 ml-16 font-weight-light"><span class="grey--text">Keycloak</span><img style="height:30px;" src="https://upload.wikimedia.org/wikipedia/commons/2/29/Keycloak_Logo.png"/> is used as an authorization server with the OpenID Connect protocol.</h2>
+          <h3 class="mt-16 ml-16 red--text font-weight-light">The blog development is still in progress...</h3>
       </v-col>
     </v-row>
+    <v-row class="mx-16">
+      <v-col cols="10" class="mx-16">
+        <v-btn
+          href="https://icons8.com/icon/90519/spring-logo"
+          text
+          small
+          class="mx-16 mt-16"
+        >
+        Icons by Icons8
+        </v-btn>
+      </v-col>
+    </v-row>
+    
   </v-container>
 </template>
