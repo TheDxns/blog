d64760f1f33d4ce4bbb33a010940e6006d78c356
TheDxns <denis.lukasczyk@gmail.com>
2021-10-11 19:53:30 +0200

    corrected keycloak functionalities in components using now the vue-keycloak plugin

--- src/frontend/src/components/UserMenu.vue
+++ src/frontend/src/components/UserMenu.vue
@@ -24,7 +24,6 @@
               size="48"
               v-if="!dataLoading"
             >
-              <!--TODO: Replace icon with user avatar-->
               <v-icon>mdi-account</v-icon>
             </v-avatar>
             <v-avatar
@@ -47,12 +46,11 @@
               <v-avatar
                 color="blue-grey"
               >
-                <!--TODO: Replace icon with user avatar-->
                 <v-icon color="white">mdi-account</v-icon>
               </v-avatar>
-              <h3 class="mt-2">{{ user.username }}</h3>
+              <h3 class="mt-2">{{ $keycloak.idTokenParsed.name }}</h3>
               <p class="text-caption mt-1">
-                {{ user.email }}
+                {{ $keycloak.idTokenParsed.email }}
               </p>
               <v-divider class="my-3"></v-divider>
               <v-btn
@@ -75,7 +73,7 @@
               <v-divider class="my-3"></v-divider>
               <v-btn
                 
-                @click="logOut()"
+                @click="$keycloak.logoutFn"
                 depressed
                 rounded
                 text
@@ -93,32 +91,12 @@
     name: "UserMenu",
      data() {
       return {
-        dataLoading: true,
-        user: {
-          initials: '',
-          email: '',
-          username: ''
+        dataLoading: false
         }
-    }
     },
     methods: {
-      logOut() {
-          document.location.replace("http://localhost:8180/auth/realms/Blog/protocol/openid-connect/logout?redirect_uri=http://localhost:3000/");
-          this.getUserInfo();
-      },
-      getUserInfo() {
-        setTimeout(() => {
-          this.user.username = this.$userInfo.preferred_username; 
-          this.user.roles = this.$userInfo.preferred_username
-          this.user.initials = "U1";
-          this.user.email = "user1@gmail.com";
-          //TODO: check if the data is loaded, if not wait some more
-          this.dataLoading = false;
-        }, 300);
-      }
     },
     mounted() {
-      this.getUserInfo();
     }
   }
 </script>
