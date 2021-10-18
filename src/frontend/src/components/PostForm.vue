a92bebc6309fbd1e570a3baad4e8becbb3668926
TheDxns <denis.lukasczyk@gmail.com>
2021-10-18 15:34:28 +0200

    updated post creation form

--- src/frontend/src/components/PostForm.vue
+++ src/frontend/src/components/PostForm.vue
@@ -1,29 +1,47 @@
 <template>
   <v-form 
   class="mx-16 d-flex flex-column justify-center"
-  style="width:50%"
   >
+  <v-row
+  style="width:50%"
+  class="mt-10">
       <v-text-field
           v-model="postTitle"
           :counter="30"
           label="Title"
           required
       ></v-text-field>
+      </v-row>
+      <v-row
+      style="width:50%">
       <v-select
           v-model="postCategory"
           :items="categories"
           label="Category"
           required
       ></v-select>
-      <wysiwyg v-model="postContent" />
+    </v-row>
+    <v-row
+      style="width:75%">
+      <wysiwyg v-model="postContent" class="mt-10" />
+    </v-row>
+    <v-row
+      style="width:75%">
+      <v-checkbox
+      v-model="postIsFeatured"
+      :label="'Feature post?'"
+      class="mt-5"
+      ></v-checkbox>
+      </v-row>
       <v-btn
           style="width:15%"
           color="teal"
-          class="ma-2 white--text"
+          class="ma-2 mt-5 white--text"
           @click="doPost"
       >
         Publish post
       </v-btn>
+
   </v-form>
 </template>
 <script>
@@ -36,6 +54,7 @@ export default {
       postTitle: '',
       postContent: '',
       postCategory: '',
+      postIsFeatured: false,
       categories: [
         'Test',
         'Other'
@@ -55,7 +74,7 @@ export default {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
-        body:JSON.stringify({title:this.postTitle, content:this.postContent, category:this.postCategory, creatorId: this.$keycloak.idTokenParsed.sub})
+        body:JSON.stringify({title:this.postTitle, content:this.postContent, category:this.postCategory, creatorId: this.$keycloak.idTokenParsed.sub, creatorUsername: this.$keycloak.idTokenParsed.preferred_username, featured: this.postIsFeatured})
       }).then(response => response.text())
         .then((response) => {
             console.log(response);
