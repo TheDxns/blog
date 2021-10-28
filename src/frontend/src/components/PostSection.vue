fec59b1c70090d70843cd3dc93ae37cc18f4e9fc
TheDxns <denis.lukasczyk@gmail.com>
2021-10-28 14:36:17 +0200

    design corrections

--- src/frontend/src/components/PostSection.vue
+++ src/frontend/src/components/PostSection.vue
@@ -1,6 +1,5 @@
 <template>
   <v-container>
-        <h1 class="mt-6 ml-16 font-weight-light">Latest posts:</h1>
         <v-row v-if="setFilter" class="ml-16 mt-10">
             <p>Sorting by {{sortingType}}: <b>{{filter}}</b></p><v-btn small class="ml-5 red--text" text v-on:click="removeFilter()">Reset</v-btn> 
         </v-row>
@@ -48,7 +47,6 @@ import Post from '@/components/Post.vue'
     },
     created() {
       this.getPosts();
-      
     },
     computed: {
             totalPages: function() {
