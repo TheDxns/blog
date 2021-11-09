8838a0e5cd807e461ccac2bc1e2f1d0ed6ea356f
TheDxns <denis.lukasczyk@gmail.com>
2021-11-09 10:17:32 +0100

    updated unit tests for main class

--- src/test/java/io/github/thedxns/blog/BlogApplicationTests.java
+++ src/test/java/io/github/thedxns/blog/BlogApplicationTests.java
@@ -1,13 +1,20 @@
 package io.github.thedxns.blog;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Mock
+    BlogApplication blogApplication;
 
+    @Test
+    @DisplayName("The context should not be null")
+    void contextLoads() throws Exception {
+		Assertions.assertNotNull(blogApplication);
+    }
 }
