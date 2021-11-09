d73cb4c7363033837e8f2f0358e9d3758173b0fd
TheDxns <denis.lukasczyk@gmail.com>
2021-11-09 10:15:06 +0100

    created unit tests for mail controller

---
+++ src/test/java/io/github/thedxns/blog/MailControllerTests.java
@@ -0,0 +1,46 @@
+package io.github.thedxns.blog;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import io.github.thedxns.blog.controller.MailController;
+import io.github.thedxns.blog.logic.MailService;
+import io.github.thedxns.blog.pojos.MailBody;
+
+@SpringBootTest
+public class MailControllerTests {
+    @Mock
+	private MailController mailController;
+
+	@Test
+    @DisplayName("The context should not be null")
+	public void contextLoads() throws Exception {
+		Assertions.assertNotNull(mailController);
+    }
+
+    @Test
+    @DisplayName("Send mail endpoint should return 200 if the mail was sent")
+    public void sendMailWhenTheMailWasSentReturns200Response() throws Exception {
+        MailService mockMailService = mock(MailService.class);
+        when(mockMailService.sendMail(any(MailBody.class))).thenReturn(true);
+        MailController testMailController = new MailController(mockMailService);
+        Assertions.assertEquals(ResponseEntity.ok().build(), testMailController.contact(new MailBody()));
+    }
+
+    @Test
+    @DisplayName("Send mail endpoint should return 500 if the mail not was sent")
+    public void sendMailWhenTheMailWasSentReturns500Response() throws Exception {
+        MailService mockMailService = mock(MailService.class);
+        when(mockMailService.sendMail(any(MailBody.class))).thenReturn(false);
+        MailController testMailController = new MailController(mockMailService);
+        Assertions.assertEquals(ResponseEntity.internalServerError().build(), testMailController.contact(new MailBody()));
+    }
+}
