30e3ee8ede3e614f753971888a13e154533d9ca1
TheDxns <denis.lukasczyk@gmail.com>
2021-11-16 12:46:29 +0100

    added newsletter widget from SendGrid

--- src/frontend/src/views/Home.vue
+++ src/frontend/src/views/Home.vue
@@ -12,38 +12,24 @@
           <v-row>
             <v-col
             cols:12>
-              <h1 class="mt-10 ml-5 font-weight-light">Worth reading:</h1>
+              <h1 class="mt-10 ml-13 font-weight-light">Worth reading:</h1>
             </v-col>
           </v-row>
           <v-row>
             <v-col
             cols:12>
               <ul>
-                <li v-for="post in posts" :key="post.title" v-bind:post="post" style="overflow:hidden;">
+                <li class="mb-3" v-for="post in posts" :key="post.title" v-bind:post="post" style="overflow:hidden;">
                 <a style="text-decoration:none;" class="blue-grey--text" :href="'http://localhost:3000/posts/' + post.id" >{{post.title}}</a>
                 </li>
               </ul>
-              
             </v-col>
           </v-row>
-          <v-row>
+          <v-row class="mt-5" style="height:570px">
             <v-col
             cols:12>
-              <h1 class="mt-10 ml-5 font-weight-light">Most popular:</h1>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col
-            cols:12>
-              <ul>
-                <li style="overflow:hidden;">
-                <a style="text-decoration:none;" class="blue-grey--text" href="/">Dolor proident ullamco laborum aliquip sint laborum</a><br />
-                <a style="text-decoration:none;" class="blue-grey--text" href="/">Et Lorem anim ea laborum Lorem Lorem</a><br />
-                <a style="text-decoration:none;" class="blue-grey--text" href="/">Et fugiat eu ad laborum excepteur eu est sint voluptate</a><br />
-                <a style="text-decoration:none;" class="blue-grey--text" href="/">A new lorem ipsum post</a><br />
-                </li>
-              </ul>
-              
+              <h1 class="ml-13 font-weight-light">Newsletter</h1>
+              <iframe frameBorder="0" style="width: 100%; height:100%; padding-right:90px;" src="https://cdn.forms-content.sg-form.com/15645814-46c4-11ec-b6ca-4200ccaabc3a"/>
             </v-col>
           </v-row>
       </v-col>
@@ -72,3 +58,4 @@ export default {
     },
 }
 </script>
+
