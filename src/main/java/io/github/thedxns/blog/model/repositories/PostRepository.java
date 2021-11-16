6099b2d619d83c79e465e34b116eada918900862
TheDxns <denis.lukasczyk@gmail.com>
2021-11-16 13:03:16 +0100

    corrected whitespaces, removed empty lines

--- src/main/java/io/github/thedxns/blog/model/repositories/PostRepository.java
+++ src/main/java/io/github/thedxns/blog/model/repositories/PostRepository.java
@@ -1,10 +1,8 @@
 package io.github.thedxns.blog.model.repositories;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import io.github.thedxns.blog.model.Post;
 
 @Repository
