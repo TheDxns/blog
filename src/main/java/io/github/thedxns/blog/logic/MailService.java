2b6be28b5522d41e0db8c6b0a3ff3878741b2442
TheDxns <denis.lukasczyk@gmail.com>
2021-11-17 21:35:18 +0100

    created logic for contacting the newsletter subscribers

--- src/main/java/io/github/thedxns/blog/logic/MailService.java
+++ src/main/java/io/github/thedxns/blog/logic/MailService.java
@@ -9,9 +9,12 @@ import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.Personalization;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 @Service
 public class MailService {
@@ -43,5 +46,54 @@ public class MailService {
         } catch (IOException ex) {
           throw ex;
         }
-        }
+    }
+
+    public boolean notifySubscribers(MailBody mailBody) throws Exception {
+      SendGrid sg = new SendGrid(apiKey);
+      Email from = new Email("denis.lukasczyk@gmail.com");
+      String subject = mailBody.getSubject();
+      Content content = new Content("text/plain", mailBody.getContent());
+      Personalization personalization = new Personalization();
+      Request getRequest = new Request();
+      getRequest.setMethod(Method.GET);
+      getRequest.setEndpoint("/marketing/contacts");
+      getRequest.addQueryParam("page", "1");
+      getRequest.addQueryParam("page_size", "1");
+
+      Response response = null;
+      try {
+          response = sg.api(getRequest);
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+      if (response != null) {
+          System.out.println(response.getBody());
+      }
+
+      JSONObject getResponse = new JSONObject(response);
+      JSONObject responseBody = new JSONObject(getResponse.getString("body"));
+      JSONArray recipientsData = new JSONArray(responseBody.getJSONArray("result").toString());
+      for (int i=0; i<recipientsData.length(); i++) {
+        personalization.addTo(new Email(recipientsData.getJSONObject(i).getString("email")));
+      }
+      
+      Mail mail = new Mail();
+      mail.addPersonalization(personalization);
+      mail.setFrom(from);
+      mail.setSubject(subject);
+      mail.addContent(content);
+      Request postRequest = new Request();
+      try {
+        postRequest.setMethod(Method.POST);
+        postRequest.setEndpoint("mail/send");
+        postRequest.setBody(mail.build());
+        response = sg.api(postRequest);
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getBody());
+        System.out.println(response.getHeaders());
+        return true;
+      } catch (IOException ex) {
+        throw ex;
+      }
+  }
 }
